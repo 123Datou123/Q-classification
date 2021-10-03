787ca1eda93e39137836968acc911874eb4b585a
JumpingRain <1826168699@qq.com>
2021-10-03 12:13:54 +0800

    1

--- app.py
+++ app.py
@@ -22,6 +22,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+
+
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop.')
 def initdb(drop):
@@ -38,24 +40,11 @@ def forge():
     db.create_all()
 
     name = 'Grey Li'
-    movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
-    ]
 
     user = User(name=name)
+    # res = 0    
     db.session.add(user)
-    for m in movies:
-        movie = Movie(title=m['title'], year=m['year'])
-        db.session.add(movie)
+    # db.session.add(res)
 
     db.session.commit()
     click.echo('Done.')
@@ -66,10 +55,10 @@ class User(db.Model):
     name = db.Column(db.String(20))
 
 
-class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(60))
-    year = db.Column(db.String(4))
+# class Movie(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(60))
+#     year = db.Column(db.String(4))
 
 
 @app.context_processor
@@ -86,48 +75,15 @@ def page_not_found(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        title = request.form['title']
-        year = request.form['year']
+        x = int(request.form['x'])
+        y = int(request.form['y'])
 
-        if not title or not year or len(year) > 4 or len(title) > 60:
+        if not x > 0 or not y >0:
             flash('Invalid input.')
             return redirect(url_for('index'))
+        res = x*y
+        # return redirect(url_for('index'))
+        return render_template('index.html',res = res)
 
-        movie = Movie(title=title, year=year)
-        db.session.add(movie)
-        db.session.commit()
-        flash('Item created.')
-        return redirect(url_for('index'))
-
-    movies = Movie.query.all()
-    return render_template('index.html', movies=movies)
-
-
-@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
-def edit(movie_id):
-    movie = Movie.query.get_or_404(movie_id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        year = request.form['year']
-
-        if not title or not year or len(year) > 4 or len(title) > 60:
-            flash('Invalid input.')
-            return redirect(url_for('edit', movie_id=movie_id))
-
-        movie.title = title
-        movie.year = year
-        db.session.commit()
-        flash('Item updated.')
-        return redirect(url_for('index'))
-
-    return render_template('edit.html', movie=movie)
-
-
-@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
-def delete(movie_id):
-    movie = Movie.query.get_or_404(movie_id)
-    db.session.delete(movie)
-    db.session.commit()
-    flash('Item deleted.')
-    return redirect(url_for('index'))
+    # movies = Movie.query.all()
+    return render_template('index.html')
