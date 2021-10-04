6a413c1cd7415dca1196fe9c4b442b167c3d9bf6
Simon <siminl0331@outlook.com>
2021-10-04 16:33:47 +0800

    Ready to deploy

---
+++ config.py
@@ -0,0 +1,2 @@
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
